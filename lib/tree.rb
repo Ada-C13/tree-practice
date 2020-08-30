cd06ffc5ba3038cec255b364d285fff64f69db4f
Yieni <yaynee8d@gmail.com>
2020-08-30 14:03:53 -0700

    trying to write inorder

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,8 +16,8 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def add(key, value)
     new_node = TreeNode.new(key,value)
     if @root.nil?
@@ -39,35 +39,47 @@ class Tree
     return current
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log n)
+  # Space Complexity: O(1)
   def find(key)
     current = @root
     return nil if current.nil?
     
     until current == nil
       if current.key == key
-        return current.key
+        return current.value
       elsif key < current.key
         current = current.left
       else
         current = current.right
       end
     end
-
     return nil
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    traverse_array = []
+    return traverse_array if @root.nil?
+
+    in_order_helper(node =@root) do |node|
+      traverse_array << {:key=> node.key, :value=>node.value}
+    end
+
+    return traverse_array
+  end
+
+  def in_order_helper(node, &block)
+    in_order_helper(node.left, &block)
+    yield node
+    in_order_helper(node.right, &block)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+
   end
 
   # Time Complexity: 
@@ -94,3 +106,25 @@ class Tree
     return "#{self.inorder}"
   end
 end
+
+tree = Tree.new
+tree.add(5, "Peter")
+tree.add(3, "Paul")
+tree.add(1, "Mary")
+tree.add(10, "Karla")
+tree.add(15, "Ada")
+tree.add(25, "Kari")
+p tree 
+# tree.inorder
+
+# balanced = Tree.new
+# balanced.add(5, "Peter")
+# balanced.add(3, "Paul")
+# balanced.add(4, "Kate")
+# balanced.add(1, "Mary")
+# balanced.add(10, "Karla")
+# balanced.add(8, "Ada")
+# balanced.add(25, "Kari")
+
+# p balanced.inorder
+    
