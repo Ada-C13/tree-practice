85c4c5ecf6a18c69b532f0b58110caa15dbaafd4
Maryam Heshmati <mairheshmati@gmail.com>
2020-08-30 15:08:07 -0700

    added add/find functionality

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,13 +19,46 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+
+    if @root.nil?
+      @root = new_node
+    else
+      add_helper(@root, new_node)
+    end
+  end
+
+  def add_helper(current, new_node)
+    return new_node if current.nil?
+
+    if new_node.key <= current.key
+      current.left = add_helper(current.left, new_node)
+    else
+      current.right = add_helper(current.right, new_node)
+    end
+    return current 
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    if @root.nil?
+      return nil 
+    elsif @root.key == key 
+      return @root.value
+    else
+      find_helper(@root, key)
+    end
+  end
+
+  def find_helper(current, key)
+    return current.value if current.key == key 
+
+    if key <= current.key
+      find_helper(current.left, key)
+    else
+      find_helper(current.right, key)
+    end
   end
 
   # Time Complexity: 
