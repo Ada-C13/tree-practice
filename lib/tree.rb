3f2d4e674853d8bd266716114339cd6d8d5db4cf
Jessica - Ada <jessica-ada@osxltTML86.local>
2020-08-30 19:09:30 -0700

    funcs

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,37 +19,118 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+
+    if @root == nil
+      @root = new_node
+    else
+      add_helper(new_node)
+    end
+  end
+
+  def add_helper(current = @root, new_node)
+    return new_node if current == nil
+
+    if new_node.key < current.key
+      current.left = add_helper(current.left, new_node)
+    else
+      current.right = add_helper(current.right, new_node);
+    end
+
+    return current
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    current = @root
+    return nil if current == nil
+
+    while current != nil
+      if key < current.key
+        current = current.left
+      elsif key > current.key
+        current = current.right
+      else
+        return current.value
+      end
+    end
+
+    return nil # not found
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    arr = []
+    current = @root
+
+    inorder_helper(arr, current)
+  end
+  
+  def inorder_helper(arr, current)
+    return arr if current == nil
+
+    inorder_helper(arr, current.left) # left
+    arr << {:key => current.key, :value => current.value} # current
+    inorder_helper(arr, current.right) # right
+
+    return arr
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    arr = []
+    current = @root
+
+    preorder_helper(arr, current)
+  end
+
+  def preorder_helper(arr, current)
+    return arr if current == nil
+
+    arr << {:key => current.key, :value => current.value} # current
+    preorder_helper(arr, current.left) # left
+    preorder_helper(arr, current.right) # right
+
+    return arr
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    arr = []
+    current = @root
+
+    postorder_helper(arr, current)
+  end
+
+  def postorder_helper(arr, current)
+    return arr if current == nil
+    
+    postorder_helper(arr, current.left) # left
+    postorder_helper(arr, current.right) # right
+    arr << {:key => current.key, :value => current.value} # current
+
+    return arr
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
-    raise NotImplementedError
+    return height_finder(0, current = @root)
+  end
+
+  def height_finder(height, current)
+    return height if current == nil
+
+    height += 1 
+
+    left = height_finder(height, current.left)
+    right = height_finder(height, current.right)
+
+    return left > right ? left : right
   end
 
   # Optional Method
