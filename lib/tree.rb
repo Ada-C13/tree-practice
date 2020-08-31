f2143e36c9a4222eb6c1d01f20f7365fd3c5b0c1
Rosalyn <syrosalynyu@gmail.com>
2020-08-30 22:41:58 -0700

    complete required method and pass the tests

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,12 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
     @left = nil
     @right = nil
-   end
+  end
 end
 
 class Tree
@@ -19,37 +19,134 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+    
+    if @root.nil?
+      @root = new_node
+      return
+    end
+
+    return add_helper(@root, new_node)
+  end
+
+  def add_helper(current, new_node)
+    return new_node if current.nil?
+
+    if new_node.key <= current.key
+      current.left = add_helper(current.left, new_node)
+    else
+      current.right = add_helper(current.right, new_node)
+    end
+    return current
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return nil if @root.nil?
+    return find_helper(@root, key)
+  end
+
+  def find_helper(current, key)
+    return current.value if current.key == key
+
+    if key <= current.key
+      return nil if current.left == nil
+      current.left = find_helper(current.left, key)
+    else
+      return nil if current.right == nil
+      current.right = find_helper(current.right, key)
+    end
+
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    result = []
+    return inorder_helper(@root, result)
+  end
+
+  def inorder_helper(node, result)
+    return if node.nil?
+  
+    if node.left
+      inorder_helper(node.left, result) 
+    end
+    result << { :key => node.key, :value => node.value }
+    if node.right
+      inorder_helper(node.right, result) 
+    end
+
+    return result
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    result = []
+    return preorder_helper(@root, result)
+  end
+
+  def preorder_helper(node, result)
+    return if node.nil?
+  
+    result << { :key => node.key, :value => node.value }
+
+    if node.left
+      preorder_helper(node.left, result) 
+    end
+    # result << { :key => node.key, :value => node.value }
+    if node.right
+      preorder_helper(node.right, result) 
+    end
+
+    return result
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    result = []
+    return postorder_helper(@root, result)
+  end
+
+  def postorder_helper(node, result)
+    return if node.nil?
+  
+    if node.left
+      postorder_helper(node.left, result) 
+    end
+    # result << { :key => node.key, :value => node.value }
+    if node.right
+      postorder_helper(node.right, result) 
+    end
+    result << { :key => node.key, :value => node.value }
+
+    return result
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
-    raise NotImplementedError
+    return 0 if @root.nil?
+    return hegiht_helper(@root)
+  end
+
+  def hegiht_helper(node)
+    return 0 if node.nil?
+
+    left_tree = hegiht_helper(node.left)
+    right_tree = hegiht_helper(node.right)
+  
+    if left_tree > right_tree
+      return left_tree + 1
+    else
+      return right_tree + 1
+    end
   end
 
   # Optional Method
@@ -64,3 +161,12 @@ class Tree
     return "#{self.inorder}"
   end
 end
+
+# if node.left 
+#   inorder_helper(node.left, result)
+#   result << node
+# end
+
+# if node.right
+#   inorder_helper(node.right, result << node.right.value)
+# end
