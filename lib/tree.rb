543eb3f888c16441a89f1643f5cb3c162b5b4206
SuelyBarreto <suely@barreto.us>
2020-08-30 17:40:37 -0700

    Tests passing

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,29 @@
 class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
-
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
-    @left = nil
-    @right = nil
-   end
+    @left, @right = nil, nil
+  end
+  def inorder_list(list) # (left, root, right)
+    list = @left.inorder_list(list) unless @left.nil?
+    list << {key: @key, value: @value}
+    list = @right.inorder_list(list) unless @right.nil?
+    return list
+  end
+  def preorder_list(list) # (root, left, right)
+    list << {key: @key, value: @value}
+    list = @left.preorder_list(list) unless @left.nil?
+    list = @right.preorder_list(list) unless @right.nil?
+    return list
+  end
+  def postorder_list(list) # (left, right , root)
+    list = @left.postorder_list(list) unless @left.nil?
+    list = @right.postorder_list(list) unless @right.nil?
+    list << {key: @key, value: @value}
+    return list
+  end
 end
 
 class Tree
@@ -16,51 +32,84 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # add(value) - adds a value to the Binary Search Tree
+  # Time Complexity: O(n)
+  # Space Complexity: O(1)
   def add(key, value)
-    raise NotImplementedError
+    if @root.nil?
+      @root = TreeNode.new(key, value)
+    else
+      add_on_node(@root, key, value)
+    end
+  end
+  def add_on_node(node, key, value)
+    if node.nil?
+      return TreeNode.new(key, value)
+    else
+      if node.key > key
+        node.left = add_on_node(node.left, key, value)
+      else
+        node.right = add_on_node(node.right, key, value)
+      end
+    end
+    return node
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # find(value) - returns true if the given value is in the tree and false otherwise.
+  # Time Complexity: O(n)
+  # Space Complexity: O(1)
   def find(key)
-    raise NotImplementedError
+    node = @root
+    until node.nil? do
+      if node.key == key
+        return node.value
+      elsif key < node.key
+        node = node.left
+      else
+        node = node.right
+      end
+    end
+    return nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # inorder - returns an array of all the elements in the tree, in order.
+  # Time Complexity: O(n) because you visit each node
+  # Space Complexity: O(n)
   def inorder
-    raise NotImplementedError
+    result = []
+    return result if @root.nil?
+    return @root.inorder_list(result)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # preorder - returns an array of all the elements in a preorder fashion (root, left, right).
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def preorder
-    raise NotImplementedError
+    result = []
+    return result if @root.nil?
+    return @root.preorder_list(result)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # postorder - returns an array of all the elements in a postorder fashion (left, right , root).
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def postorder
-    raise NotImplementedError
+    result = []
+    return result if @root.nil?
+    return @root.postorder_list(result)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n) if not balanced
+  def height_helper(node)
+    return 0 if node.nil?
+    if height_helper(node.right) > height_helper(node.left)
+      return height_helper(node.right) + 1
+    else
+      return height_helper(node.left) + 1
+    end
+  end 
   def height
-    raise NotImplementedError
-  end
-
-  # Optional Method
-  # Time Complexity: 
-  # Space Complexity: 
-  def bfs
-    raise NotImplementedError
-  end
-
-  # Useful for printing
-  def to_s
-    return "#{self.inorder}"
+    return height_helper(@root)
   end
 end
