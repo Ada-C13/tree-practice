0bb1a1c145e231c7da242657387540cadbb177c0
peachmakkoli <lee.higgins.dev@gmail.com>
2020-08-31 00:27:15 -0700

    writes add, find, and inorder methods plus helpers

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,37 +19,91 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+    return @root = new_node if !@root
+    add_helper(@root, new_node)
+  end
+
+  def add_helper(current, new_node)
+    if new_node.key <= current.key
+      return current.left = new_node if !current.left
+      add_helper(current.left, new_node)
+    else
+      return current.right = new_node if !current.right
+      add_helper(current.right, new_node)
+    end 
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return if !@root
+    return @root.value if @root.key == key
+    
+    if key <= @root.key
+      find_helper(key, @root.left)
+    else 
+      find_helper(key, @root.right)
+    end
+  end
+
+  def find_helper(key, current)
+    return current.value if current.key == key
+    if key <= current.key
+      find_helper(key, current.left)
+    else 
+      find_helper(key, current.right)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return [] if !@root
+    nodes = []
+    inorder_helper(@root, nodes)
+    return nodes
+  end
+
+  def inorder_helper(current, nodes)
+    return if !current
+    # traverse the left subtree
+    inorder_helper(current.left, nodes)
+    # visit the current node
+    nodes.push(
+      {
+        key: current.key,
+        value: current.value
+      }
+    )
+    #traverse the right subtree
+    inorder_helper(current.right, nodes)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
     raise NotImplementedError
+    # visit the current node
+    # traverse the left subtree
+    # traverse the right subtree
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
     raise NotImplementedError
+    # traverse the left subtree
+    # traverse the right subtree
+    # visit the current node
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
     raise NotImplementedError
+    # If the current node is nil return 0
+    # Otherwise return 1 plus the maximum of the heights of the right and left subtrees
   end
 
   # Optional Method
